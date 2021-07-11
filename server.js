4f014ff16a7c195c4e3e01cce6ea911b40655aac
Nagendra Yadav <nagendrayadav17423@gmail.com>
2021-07-11 17:09:20 +0530

    running app condition

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/news-app'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/news-app/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
+
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
